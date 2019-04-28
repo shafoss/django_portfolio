3d5edf44acddf1923dd2eeb4c7c534a9b421d198
Shannon Foss <shannonrfoss@gmail.com>
2019-04-27 22:37:16 -0500

    Fixed unexpected unindent error in setup.py due to unnecessary nested try blocks.

--- setup_db.py
+++ setup_db.py
@@ -16,28 +16,26 @@ def setup_database():
     try:
         conn = psycopg2.connect(
             "dbname='postgres' user='" + user + "' password='" + password + "' host='localhost' port='5432'")
-        try:
-            # set the proper isolation level so CREATE DATABASE works
-            conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-            with conn.cursor() as cur:
-                # execute SQL in here
-                cur.execute("CREATE DATABASE " + dbname)
-                cur.execute("GRANT ALL PRIVILEGES ON DATABASE " + dbname + " TO " + user)
+        # set the proper isolation level so CREATE DATABASE works
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        with conn.cursor() as cur:
+            # execute SQL in here
+            cur.execute("CREATE DATABASE " + dbname)
+            cur.execute("GRANT ALL PRIVILEGES ON DATABASE " + dbname + " TO " + user)
     except OperationalError:
         # if the user defined in settings hasn't been created yet, log in with a valid user instead.
         other_user = input("Enter username: ")
         other_password = input("Enter password: ")
         conn = psycopg2.connect(
             "dbname='postgres' user='" + other_user + "' password='" + other_password + "' host='localhost' port='5432'")
-        try:
-            # set the proper isolation level so CREATE DATABASE works
-            conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-            with conn.cursor() as cur:
-                # execute SQL in here
-                cur.execute("CREATE ROLE " + user + " WITH LOGIN PASSWORD '" + password + "'")
-                cur.execute("ALTER USER " + user + " CREATEDB")
-                cur.execute("CREATE DATABASE " + dbname)
-                cur.execute("GRANT ALL PRIVILEGES ON DATABASE " + dbname + " TO " + user)
+        # set the proper isolation level so CREATE DATABASE works
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        with conn.cursor() as cur:
+            # execute SQL in here
+            cur.execute("CREATE ROLE " + user + " WITH LOGIN PASSWORD '" + password + "'")
+            cur.execute("ALTER USER " + user + " CREATEDB")
+            cur.execute("CREATE DATABASE " + dbname)
+            cur.execute("GRANT ALL PRIVILEGES ON DATABASE " + dbname + " TO " + user)
     finally:
         conn.close()
 
